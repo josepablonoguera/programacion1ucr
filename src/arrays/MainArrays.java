adb5e91b746c4c4dc53f118b7840304f3f3230a9
Profe UCR <ucr@profe.com>
2023-09-22 15:20:45 -0600

    Arrays

---
+++ src/arrays/MainArrays.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ucr
+ */
+public class MainArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int notas [] = {90,100,50,100,90};
+        
+//        System.out.println("------");
+//        for (int j = 0; j < notas.length; j++) {
+//          System.out.println("notas:["+j+"] = "+notas[j]);
+//        }
+        
+        int sumatoria =0;
+        for (int i = 0; i <= notas.length; i++) {
+           sumatoria+=notas[i];
+        }
+        int promedio = 0;
+        
+        promedio = sumatoria/ notas.length;
+        System.out.println("promedio: "+promedio);
+        
+           Arrays.sort(notas);
+           
+           System.out.println("");
+    
+           System.out.println("------");
+        for (int j = 0; j < notas.length; j++) {
+          System.out.println("notas:["+j+"] = "+notas[j]);
+        }
+    }
+    
+ 
+}
---
+++ src/arrays/MatricesMain.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arrays;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ucr
+ */
+public class MatricesMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tablero [][] = {
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8},
+            {1,2,3,4,5,6,7,8}
+        };
+        
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                System.out.print(" "+tablero[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("------");
+        Arrays.sort(tablero);
+        System.out.println("");
+         for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                System.out.print(" "+tablero[i][j]);
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
